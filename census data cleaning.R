78512e79e5d4d9edf561472cc6dc109b205a5486
matthewjnicholsn <nichom18@mcmaster.ca>
2025-06-18 11:25:38 -0400

    Need to fix the loop, naming a temporary object and then saying it does not exist

--- census data cleaning.R
+++ census data cleaning.R
@@ -4,7 +4,17 @@ library(dplyr)
 library(ggplot2)
 library(stringr)
 
-
+#fix names of birthplace
+mapping <- data.frame(
+  birthplace_2011 = c("Total - Place of birth of respondent", "Born in Canada", 
+                      "Born outside Canada", "Africa", "Western Africa", 
+                      "Eastern Africa", "Northern Africa", "Central Africa", 
+                      "Southern Africa", "Africa, n.i.e."),
+  birthplace_2021 = c("Total - Place of birth", "Born in Canada", 
+                      "Born outside Canada", "Africa", "Western Africa", 
+                      "Eastern Africa", "Northern Africa", "Central Africa", 
+                      "Southern Africa", NA)  # NA for "Africa, n.i.e."
+)
 
 data <- c("datatable2011.csv", "datatable2021.csv")
 
@@ -20,6 +30,8 @@ for(i in seq_along(data)) {
   df <- df %>% 
     mutate_if(is.character, ~ iconv(., from = "UTF-8", to = "UTF-8"))
   
+  
+  
   # rename vars
   if (year == "2011") {
     df <- df %>% 
@@ -32,13 +44,13 @@ for(i in seq_along(data)) {
       mutate(counts = as.numeric(counts)) %>% 
       mutate(birthplace = as.factor(birthplace)) %>% 
       mutate(birthplace = as.factor(birthplace)) %>% 
-      filter(!str_detect(labour, "Total"))
+      mutate(across(where(is.character), trimws)) %>% 
+      filter(!str_detect(labour, "Total")) %>% 
+      pivot_wider(names_from = labour, values_from = counts, 
+                  values_fill = list(counts = 0))
     
     #trying to remove the non printable characters
     df$labour <- gsub("[^[:print:]]", "", df$labour)
-    df <- df %>% 
-      pivot_wider(names_from = labour, values_from = counts, 
-                  values_fill = list(counts = 0))
     
   } else if (year == "2021") {
     df <- df %>% 
@@ -50,16 +62,16 @@ for(i in seq_along(data)) {
       mutate(year = as.numeric(year)) %>% 
       mutate(counts = as.numeric(counts)) %>% 
       mutate(birthplace = as.factor(birthplace)) %>% 
-      filter(!str_detect(labour, "Total"))
-    
-    #trying to remove the non printable characters
-    df$labour <- gsub("[^[:print:]]", "", df$labour)
-    df <- df %>% 
+      mutate(across(where(is.character), trimws)) %>% 
+      filter(!str_detect(labour, "Total")) %>% 
       pivot_wider(names_from = labour, values_from = counts, 
                   values_fill = list(counts = 0)) %>% 
       mutate(education = ifelse(is.na(education),"  Bachelor's degree or higher",
                                 education)) #this is to fix the NA's by coercion error
     
+    
+    #trying to remove the non printable characters
+    df$labour <- gsub("[^[:print:]]", "", df$labour)
     
     
   }
@@ -67,13 +79,31 @@ for(i in seq_along(data)) {
   # give df a year
   assign(paste0("df_", year), df)
 }
+
+#fix naming of birthplace for 2011 
+df_2011 <- df_2011 %>% 
+  left_join(mapping, by = c("birthplace" = "birthplace_2021")) %>% 
+  mutate(birthplace = coalesce(birthplace_2021,birthplace)) %>% 
+  select(-birthplace_2021)
+
+#double check proper naming of birthplace in 2021
+df_2021 <- df_2021 %>% 
+  left_join(mapping, by = c("birthplace" = "birthplace_2021")) %>% 
+  mutate(birthplace = coalesce(birthplace_2021, birthplace)) %>% 
+  select(-birthplace_2021)
+
+
 # combine, may not actually prove useful
 df_comb <- bind_rows(df_2011, df_2021)
 
-#get some summary tables
+#convert chr var to factors
 employ_stats <- df_comb %>% 
   select(1:4, 9:11) %>% 
   filter(as.integer(birthplace) == 4:9,
          education == "Total - Highest certificate, diploma or degree")
 # filter(as.integer(birthplace) == 4:9,
 #        ))
+
+df2 <- df_comb %>% 
+  select(1:4, 9:11) %>% 
+  filter(birthplace
